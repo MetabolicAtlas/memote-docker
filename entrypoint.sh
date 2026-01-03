f67afae01b9d5c73d564f3e5d0d9abdfa94c970b
Mihail Anton <mihail.anton@chalmers.se>
2026-01-03 22:37:02 +0100

    fix: handle base64 padding

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,10 @@ if [ -n "${GUROBI_LICENSE_B64:-}" ]; then
     python - "$GUROBI_LICENSE_B64" "$LICENSE_FILE" <<'PY'
 import base64, binascii, sys
 data, dest = sys.argv[1], sys.argv[2]
+# Add padding if omitted (len must be a multiple of 4).
+pad = (-len(data)) % 4
+if pad:
+    data += "=" * pad
 with open(dest, "wb") as fh:
     fh.write(base64.b64decode(data))
 PY
